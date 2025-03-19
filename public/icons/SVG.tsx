601ed45a46e5eb6ee7c5374024387484e4a502e5
김한빈 <gksqls0507@gmail.com>
2025-03-20 03:44:43 +0900

    🖼️ Add-assets:  이미지 및 SVG 파일 추가

--- public/icons/SVG.tsx
+++ public/icons/SVG.tsx
@@ -57,31 +57,31 @@ export const ShuffleSVG = () => (
       d="M16.75 10.25L18.9999 12.4999L16.75 14.7498"
       stroke="white"
       strokeLinecap="round"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
     <path
       d="M16.75 1.25L18.9999 3.49989L16.75 5.74978"
       stroke="white"
       strokeLinecap="round"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
     <path
       d="M1 12.5001H4.99337C5.61064 12.5001 6.21846 12.3477 6.76272 12.0564C7.30703 11.7651 7.77091 11.344 8.11345 10.8304L10.0002 8"
       stroke="white"
       strokeLinecap="round"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
     <path
       d="M1 3.5H4.99337C5.61064 3.5 6.21846 3.65239 6.76272 3.94365C7.30703 4.23496 7.77091 4.6561 8.11345 5.16969L11.8869 10.8305C12.2295 11.344 12.6933 11.7652 13.2376 12.0565C13.7819 12.3478 14.3897 12.5002 15.007 12.5002H17.5004"
       stroke="white"
       strokeLinecap="round"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
     <path
       d="M17.4989 3.5H15.0059C14.3886 3.5 13.7809 3.65235 13.2367 3.94361C12.6925 4.23487 12.2287 4.65593 11.8862 5.16952L11.4995 5.74972"
       stroke="white"
       strokeLinecap="round"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -98,9 +98,9 @@ export const BeforMusicSVG = () => (
     <path
       d="M1 15V1"
       stroke="white"
-      stroke-width="2"
+      strokeWidth="2"
       strokeLinecap="square"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -117,9 +117,9 @@ export const AfterMusicSVG = () => (
     <path
       d="M14 15V1"
       stroke="white"
-      stroke-width="2"
+      strokeWidth="2"
       strokeLinecap="square"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -155,6 +155,21 @@ export const RepeatSVG = () => (
     <path
       d="M17.5 13.75V14.25H18V13.75H17.5ZM0.646447 13.3964C0.451184 13.5917 0.451184 13.9083 0.646447 14.1036L3.82843 17.2855C4.02369 17.4808 4.34027 17.4808 4.53553 17.2855C4.7308 17.0903 4.7308 16.7737 4.53553 16.5784L1.70711 13.75L4.53553 10.9216C4.7308 10.7263 4.7308 10.4097 4.53553 10.2145C4.34027 10.0192 4.02369 10.0192 3.82843 10.2145L0.646447 13.3964ZM17 9.75V13.75H18V9.75H17ZM17.5 13.25H1V14.25H17.5V13.25Z"
       fill="white"
+    />
+  </svg>
+);
+
+export const BurgerMenuSVG = () => (
+  <svg
+    width="36"
+    height="27"
+    viewBox="0 0 36 27"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M35.846 13.4414C35.846 14.2662 35.1773 14.9349 34.3525 14.9349H1.49572C0.346046 14.9349 -0.372501 13.6904 0.202288 12.6947C0.469073 12.2326 0.962139 11.9479 1.49572 11.9479H34.3525C35.1773 11.9479 35.846 12.6166 35.846 13.4414ZM1.49572 2.98698H34.3525C35.5022 2.98693 36.2208 1.74233 35.6459 0.746693C35.3791 0.28463 34.886 0 34.3525 0H1.49572C0.346046 0 -0.372515 1.24457 0.202331 2.24023C0.469116 2.70233 0.962153 2.98698 1.49572 2.98698ZM34.3525 23.8958H1.49572C0.346046 23.8958 -0.372515 25.1404 0.202331 26.1361C0.469116 26.5982 0.962153 26.8828 1.49572 26.8828H34.3525C35.5022 26.8828 36.2208 25.6382 35.6459 24.6425C35.3791 24.1805 34.886 23.8958 34.3525 23.8958Z"
+      fill="black"
     />
   </svg>
 );
