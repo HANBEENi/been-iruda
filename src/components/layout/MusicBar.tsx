ca555f437d26f82c3f0400696f5dcccb99fa9edf
김한빈 <gksqls0507@gmail.com>
2025-03-20 17:39:57 +0900

    💄 Design:  뮤직바 퍼블리싱 완료

--- src/components/layout/MusicBar.tsx
+++ src/components/layout/MusicBar.tsx
@@ -1,3 +1,5 @@
+/* 상단 뮤직바 (셔플,이전노래,노래재생정지,다음노래,반복) */
+
 import { styled } from 'styled-components';
 import {
   AfterMusicSVG,
@@ -8,13 +10,16 @@ import {
   StopPlaySVG,
 } from '../../../public/icons/SVG';
 import { useState } from 'react';
+import { media } from '@/styles/mediaQuery';
 
 const MusicBar = () => {
   const [isMusicBarOpen, setIsMusicBarHidden] = useState<boolean>(false);
 
   return (
     <Layout $isMusicBarOpen={isMusicBarOpen}>
-      <MusicBarSVG />
+      <div className="background">
+        <MusicBarSVG />
+      </div>
       <MusicControls>
         <button>
           <ShuffleSVG />
@@ -40,24 +45,41 @@ export default MusicBar;
 
 const Layout = styled.div<{ $isMusicBarOpen: boolean }>`
   display: flex;
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%);
   justify-content: center;
   align-items: center;
   overflow: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
 
-  width: 676px;
-  height: fit-content;
+  width: 100%;
+  /* min-width: 400px; */
+  /* height: fit-content; */
 
   transition: transform 0.8s ease-in-out, opacity 0.6s ease-in-out;
   transform-origin: top; /* ✅ 아래에서 위로 줄어들도록 설정 */
   /* transform: ${({ $isMusicBarOpen }) =>
     $isMusicBarOpen ? 'scaleY(0)' : 'scaleY(1)'}; */
 
-  svg {
-    width: 100%;
-    height: 55px;
+  ${media.mobile} {
+    min-width: 300px;
+  }
+
+  .background {
+    max-width: 450px;
+    height: 100%;
+
+    svg {
+      width: 100%;
+      height: 100%;
+    }
+
+    ${media.tablet} {
+      max-width: 400px;
+    }
+    ${media.mobile} {
+      max-width: 300px;
+    }
   }
 `;
 
@@ -67,14 +89,30 @@ const MusicControls = styled.div`
   align-items: center;
   gap: 30px;
   padding-bottom: 5px;
+  z-index: 1006;
+
+  ${media.tablet} {
+    gap: 25px;
+  }
+  ${media.mobile} {
+    gap: 20px;
+  }
 
   button {
     height: 14px;
+
+    ${media.mobile} {
+      height: 12px;
+    }
     cursor: pointer;
   }
 
   .stop-play {
-    height: 44px;
+    height: 40px;
+
+    ${media.mobile} {
+      height: 30px;
+    }
   }
 
   svg {
