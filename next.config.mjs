904c10fd1b03c94784b073411ea227922d845210
김한빈 <gksqls0507@gmail.com>
2025-03-20 17:38:48 +0900

    📦️ Chore:  패키지수정(next버전,three.js추가),config.mjs수정
    - next 15 -> next 14 변경
    - three.js 버전 호환 맞춤
    - three.js 추가
    - 기존패키지호환유지하면서 three.js 사용 환경 설정을 위함

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   compiler: {
     styledComponents: true,
   },
 };
-
 export default nextConfig;
