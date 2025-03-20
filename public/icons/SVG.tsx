f005d1530497cad99e2caf1170e299fd803ea6b0
김한빈 <gksqls0507@gmail.com>
2025-03-20 20:09:22 +0900

    🖼️ Add-assets:  SVG수정 및 추가, 로고(다크모드)이미지 추가

--- public/icons/SVG.tsx
+++ public/icons/SVG.tsx
@@ -1,14 +1,17 @@
+import { useTheme } from 'styled-components';
+
+/* 뮤직바 관련 SVG */
 export const MusicBarSVG = () => (
   <svg
     width="676"
     height="78"
     viewBox="0 0 676 78"
-    fill="none"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M0 0H0.00192736H676C646.355 0 624.844 11.4222 614.107 21.34C605.96 28.8659 595.186 45.8946 583.161 60.3441C571.136 74.7941 539.95 78.1326 532.637 77.996H149.069C129.526 77.996 110.757 65.8631 104.442 60.3441C96.2996 53.2289 84.0538 35.1393 62.0074 19.7042C39.9598 4.26923 11.483 0.136828 0 0Z"
-      fill="black"
+      fill="currentColor"
     />
   </svg>
 );
@@ -23,33 +26,33 @@ export const ShuffleSVG = () => (
   >
     <path
       d="M16.75 10.25L18.9999 12.4999L16.75 14.7498"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
     <path
       d="M16.75 1.25L18.9999 3.49989L16.75 5.74978"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
     <path
       d="M1 12.5001H4.99337C5.61064 12.5001 6.21846 12.3477 6.76272 12.0564C7.30703 11.7651 7.77091 11.344 8.11345 10.8304L10.0002 8"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
     <path
       d="M1 3.5H4.99337C5.61064 3.5 6.21846 3.65239 6.76272 3.94365C7.30703 4.23496 7.77091 4.6561 8.11345 5.16969L11.8869 10.8305C12.2295 11.344 12.6933 11.7652 13.2376 12.0565C13.7819 12.3478 14.3897 12.5002 15.007 12.5002H17.5004"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
     <path
       d="M17.4989 3.5H15.0059C14.3886 3.5 13.7809 3.65235 13.2367 3.94361C12.6925 4.23487 12.2287 4.65593 11.8862 5.16952L11.4995 5.74972"
-      stroke="white"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
@@ -62,10 +65,10 @@ export const BeforMusicSVG = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M15 16L5 8L15 0V16Z" fill="white" />
+    <path d="M15 16L5 8L15 0V16Z" fill="currentColor" />
     <path
       d="M1 15V1"
-      stroke="white"
+      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="square"
       strokeLinejoin="round"
@@ -81,10 +84,10 @@ export const AfterMusicSVG = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M0 16L10 8L0 0V16Z" fill="white" />
+    <path d="M0 16L10 8L0 0V16Z" fill="currentColor" />
     <path
       d="M14 15V1"
-      stroke="white"
+      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="square"
       strokeLinejoin="round"
@@ -100,10 +103,10 @@ export const StopPlaySVG = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle cx="22" cy="22" r="22" fill="white" />
+    <circle cx="22" cy="22" r="22" fill="currentColor" />
     <path
       d="M31.2256 21.998L17.9191 30.6024L17.9191 13.3936L31.2256 21.998Z"
-      fill="black"
+      fill="#676767"
     />
   </svg>
 );
@@ -113,20 +116,21 @@ export const RepeatSVG = () => (
     width="18"
     height="18"
     viewBox="0 0 18 18"
-    fill="none"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M1 4.25V3.75H0.5V4.25H1ZM17.8536 4.60355C18.0488 4.40829 18.0488 4.09171 17.8536 3.89645L14.6716 0.714466C14.4763 0.519204 14.1597 0.519204 13.9645 0.714466C13.7692 0.909728 13.7692 1.22631 13.9645 1.42157L16.7929 4.25L13.9645 7.07843C13.7692 7.27369 13.7692 7.59027 13.9645 7.78553C14.1597 7.9808 14.4763 7.9808 14.6716 7.78553L17.8536 4.60355ZM1.5 8.25V4.25H0.5V8.25H1.5ZM1 4.75H17.5V3.75H1V4.75Z"
-      fill="white"
+      fill="currentColor"
     />
     <path
       d="M17.5 13.75V14.25H18V13.75H17.5ZM0.646447 13.3964C0.451184 13.5917 0.451184 13.9083 0.646447 14.1036L3.82843 17.2855C4.02369 17.4808 4.34027 17.4808 4.53553 17.2855C4.7308 17.0903 4.7308 16.7737 4.53553 16.5784L1.70711 13.75L4.53553 10.9216C4.7308 10.7263 4.7308 10.4097 4.53553 10.2145C4.34027 10.0192 4.02369 10.0192 3.82843 10.2145L0.646447 13.3964ZM17 9.75V13.75H18V9.75H17ZM17.5 13.25H1V14.25H17.5V13.25Z"
-      fill="white"
+      fill="currentColor"
     />
   </svg>
 );
 
+/* 버거메뉴아이콘 SVG */
 export const BurgerMenuSVG = () => (
   <svg
     width="36"
@@ -138,6 +142,24 @@ export const BurgerMenuSVG = () => (
     <path
       d="M35.846 13.4414C35.846 14.2662 35.1773 14.9349 34.3525 14.9349H1.49572C0.346046 14.9349 -0.372501 13.6904 0.202288 12.6947C0.469073 12.2326 0.962139 11.9479 1.49572 11.9479H34.3525C35.1773 11.9479 35.846 12.6166 35.846 13.4414ZM1.49572 2.98698H34.3525C35.5022 2.98693 36.2208 1.74233 35.6459 0.746693C35.3791 0.28463 34.886 0 34.3525 0H1.49572C0.346046 0 -0.372515 1.24457 0.202331 2.24023C0.469116 2.70233 0.962153 2.98698 1.49572 2.98698ZM34.3525 23.8958H1.49572C0.346046 23.8958 -0.372515 25.1404 0.202331 26.1361C0.469116 26.5982 0.962153 26.8828 1.49572 26.8828H34.3525C35.5022 26.8828 36.2208 25.6382 35.6459 24.6425C35.3791 24.1805 34.886 23.8958 34.3525 23.8958Z"
       fill="black"
+    />
+  </svg>
+);
+
+/* 아래화살표아이콘 SVG */
+export const ArrowDownSVG = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="167"
+    height="168"
+    viewBox="0 0 167 168"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M49.449 88.8842V1H119.646V88.8842H164L84.0239 166L3 88.8842H49.449Z"
+      stroke="currentColor"
+      strokeWidth="3"
     />
   </svg>
 );
