f121dee9eb5ac05d485a92c4df5d7a598aaa3243
김한빈 <gksqls0507@gmail.com>
2025-05-18 09:40:34 +0900

    ⚙️ Init: 프로젝트 리뉴얼 초기화

--- src/styles/mediaQuery.ts
+++
@@ -1,7 +0,0 @@
-const tabletQuery = () => `@media all and (max-width: 1140px)`;
-const mobileQuery = () => `@media all and (max-width: 600px)`;
-
-export const media = {
-  tablet: tabletQuery,
-  mobile: mobileQuery,
-};
