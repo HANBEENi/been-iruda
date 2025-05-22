62c1558bbdeabb50a498a67572a2490f849e9ccd
김한빈 <gksqls0507@gmail.com>
2025-05-23 02:28:38 +0900

    ✨ Feat:  프로젝트Lp내용단 재사용레이아웃구성

---
+++ src/components/common/LpCover.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react';
+import { styled } from 'styled-components';
+
+export const LpCover = ({
+  isHome = false,
+  backgroundImageUrl,
+  children,
+}: {
+  isHome?: boolean;
+  backgroundImageUrl: string;
+  children: ReactNode;
+}) => {
+  return (
+    <Layout
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+      $isHome={isHome}
+    >
+      {children}
+    </Layout>
+  );
+};
+
+const Layout = styled.div<{ $isHome: boolean }>`
+  display: flex;
+  flex-direction: column;
+  padding: ${({ $isHome }) => ($isHome ? 'none' : '20px')};
+  gap: 15px;
+  aspect-ratio: 833/826;
+  height: 100%;
+
+  box-shadow: 0 4px 40px 0 rgba(0, 0, 0, 0.05);
+  background-image: url('/images/cover-01.png');
+`;
